1c3d81b682f7818b5f958a89ef1a8ac42ac5db8a
suleyman <suleyman.sevimli1@gmail.com>
2020-08-12 17:23:19 +0300

    create dto & getTaskById

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './model/task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService:TasksService) {}
+  constructor(private tasksService: TasksService) {
+  }
 
   @Get()
-  getFindAll():Task[] {
+  getFindAll(): Task[] {
     return this.tasksService.getFindAll();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id:string): Task {
+    return this.tasksService.getTaskById(id)
+  }
+
   @Post()
-  createTask(@Body() data:Task):Task {
-    return this.tasksService.createTask(data)
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './model/task.model';
+import { Task, TaskStatus } from './model/task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks:Task[] = [];
+  private tasks: Task[] = [
+    {
+      id:"1",
+      title:"First Task",
+      description: "task description",
+      status: TaskStatus.OPEN
+    },
+    {
+      id:"2",
+      title:"Second Task",
+      description: "task description",
+      status: TaskStatus.IN_PROGRESS
+    },
+    {
+      id:"3",
+      title:"Third Task",
+      description: "task description",
+      status: TaskStatus.DONE
+    }
+  ];
 
   getFindAll(): Task[] {
     return this.tasks;
   }
 
-  createTask(data:Task):Task {
-    return data;
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    return {
+      id: '1234',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
   }
 }
